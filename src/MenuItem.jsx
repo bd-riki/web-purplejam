5dfc1253cf6c63d76f3e79fef7286818eec5df04
bd-riki <jbatarina@gmail.com>
2025-04-15 15:46:28 +0800

    Icon + Table Edits
    - Fixed Table for MenuItems to prevent table errors
    - Added icon for the webpage.

--- src/MenuItem.jsx
+++ src/MenuItem.jsx
@@ -9,17 +9,19 @@ function MenuItem(props) {
     return (
       <div className="menu-item">
         <table>
-          <tr>
-            <td className="item-container">
-              <h2 className="menu-item-name">{props.itemName}</h2>
-              <p className="menu-item-desc"><i>{props.itemDesc}</i></p>
-            </td>
-            <td>
-              <div className="price-container">
-                <p className="menu-item-price">USD {(props.itemPrice).toFixed(2)}</p>
-              </div> 
-            </td>
-          </tr>
+          <tbody>
+            <tr>
+              <td className="item-container">
+                <h2 className="menu-item-name">{props.itemName}</h2>
+                <p className="menu-item-desc"><i>{props.itemDesc}</i></p>
+              </td>
+              <td>
+                <div className="price-container">
+                  <p className="menu-item-price">USD {(props.itemPrice).toFixed(2)}</p>
+                </div> 
+              </td>
+            </tr>
+          </tbody>
         </table>
       </div>
     )
