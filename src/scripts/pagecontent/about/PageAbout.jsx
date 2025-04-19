158eec0d145de1d42055d42bf85aa097cde708ed
bd-riki <jbatarina@gmail.com>
2025-04-19 13:16:41 +0800

    "About" Section Implementation - II
    - Added later part of the section.
    - Implemented picture containers for "cropping" photos.
    
    TO-DO: add a stylized horizontal rule for the navbar spacer.

--- src/scripts/pagecontent/about/PageAbout.jsx
+++ src/scripts/pagecontent/about/PageAbout.jsx
@@ -6,27 +6,32 @@ function PageAbout() {
                 <div className="navbar-spacer">
                     {/* TO-DO: ADD HORIZONTAL DESIGN HERE */}
                 </div>
-
+                
                 <div className="ab-container">
                     <div id="pic-collage1">
-                        <img src="src/assets/jambread.jpg" alt="Purplejam Cafe" className="jambread"/>
-                        <div id="pic-collage-sub1">
-                            <img src="src/assets/jambread.jpg" alt="Purplejam Cafe" className="jambread"/>
-                            <img src="src/assets/eggtoast.jpg" alt="Purplejam Cafe" id="eggtoast"/>
+                        <div className="pic-container">
+                            <img src="src/assets/jambread.jpg" alt="Purplejam Cafe" id="jambread-wide"/>
+                        </div>
+                        <div className="pic-container">
+                            <img src="src/assets/s_pizza.jpg" alt="Purplejam Cafe" id="s_pizza"/>
                         </div>
                     </div>
                     <h3>A Nice Food Cafe in Manila...</h3>
                     <p className="ab-paragraph">Purplejam Cafe is located in the vintage streets of Escolta, Manila. Offering a relax atmosphere and great food at affordable prices, you don't have to strain your wallet when going here to eat alone or with a company of friends.</p>
-                    <p className="ab-paragraph">The cafe also offers its specialty: Purplejam Sandwich and Lavender Teas whose ingredients are sourced locally. Supporting raw ingredients made from its home country is part of ensuring quality of the shop's service.</p>
+                    <p className="ab-paragraph">The cafe also offers its specialty: Purplejam Sandwich and Lavender Teas whose ingredients are sourced locally. Importing from its home country helps of ensure quality of the shop's goods.</p>
                     
                 </div>
 
                 <div className="ab-container">
-                    {/* <div id="pic-collage2">
-                        <img src="src/assets/eggtoast.jpg" alt="Good Food" id="pic2"/>
-                        <img src="src/assets/eggtoast.jpg" alt="Good Food" id="pic3"/>
-                    </div> */}
-                    <p>Purplejam Cafe is also host to local singing talents, Be sure to check in to listen and support our gifted artists. The cafe is also open for reservations should people need a venue to hold their parties and important gatherings.</p>
+                    <div id="pic-collage2">
+                        <div className="pic-container">
+                            <img src="src/assets/jammin.jpg" alt="Purplejam Cafe" id="jammin"/>
+                        </div>
+                        <div className="pic-container">
+                            <img src="src/assets/bday.jpg" alt="Purplejam Cafe" id="bday"/>
+                        </div>
+                    </div>
+                    <p>Purplejam Cafe is also host to local singing talents, Be sure to check out, listen and support our gifted artists. The cafe is also open for reservations should people need a venue to hold their parties and important gatherings.</p>
                 </div>
                 
             </div>        
